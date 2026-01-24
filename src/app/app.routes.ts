3055d8cd8b158d9a2638ae859e426f7ce5be4447
ened-hysi <enedhysi@gmail.com>
2026-01-24 16:13:30 +0100

    Updating app.routes.ts and app.config.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,23 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
 import { CartComponent } from './features/cart/cart.component';
 import { CheckoutComponent } from './features/checkout/checkout.component';
+import { authGuard } from './core/guards/auth-guard';
+import { emptyCartGuard } from './core/guards/empty-cart-guard';
+import { OrderComponent } from './features/orders/order.component';
+import { OrderDetailedComponent } from './features/orders/order-detailed/order-detailed.component';
+import { LoginComponent } from './features/account/login/login.component';
+import { RegisterComponent } from './features/account/register/register.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
     {path: 'cart', component: CartComponent},
-    {path: 'checkout', component: CheckoutComponent},
+    {path: 'checkout', component: CheckoutComponent, canActivate: [authGuard, emptyCartGuard]},
+    {path: 'orders', component: OrderComponent , canActivate: [authGuard]},
+    {path: 'orders/:id', component: OrderDetailedComponent , canActivate: [authGuard]},
+    {path: 'account/login', component: LoginComponent},
+    {path: 'account/register', component: RegisterComponent},
     {path: 'test-error', component: TestErrorComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: 'server-error', component: ServerErrorComponent},
